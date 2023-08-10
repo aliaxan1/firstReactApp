0bcdcaadaa9cfe7f94269a831c7c41952225a0bc
Muhammad Azan Sajid <azan.developer@gmail.com>
2023-08-10 09:09:58 +0000

    includes css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 const BookList = () => {
     return (
-        <section>
+        <section className='booklist'>
             <Book />
             <Book />
             <Book />
@@ -14,7 +14,7 @@ const BookList = () => {
 
 const Book = () => {
     return (
-        <article>
+        <article className='book'>
             <Image />
             <Title />
             <Author />
